db78ab3a9ac531e5fc0e5dc3eba4f4b7c94ea697
mrdarip <darioelas2005@gmail.com>
2026-03-11 11:47:39 +0100

    Refactor Home component to display blog posts using PostsList component

---
+++ components/PostsList.tsx
@@ -0,0 +1,47 @@
+import { DataSourceObjectResponse, PageObjectResponse, PartialDataSourceObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+type Post = {
+    id: string;
+    title: string;
+    created: string;
+    lastEdited: string;
+}
+
+export default async function PostsList({ posts }: { posts:(PageObjectResponse | PartialPageObjectResponse | PartialDataSourceObjectResponse | DataSourceObjectResponse)[]}) {
+
+    const postsList = posts.map(post => {
+        if (!("properties" in post)) return null
+
+        const titleProp = post.properties["Title"]
+        if (!titleProp || titleProp.type !== "title") return null
+
+        const title = titleProp.title[0]?.plain_text || "Untitled"
+
+        const postData: Post = {
+            id: post.id,
+            title,
+            created: post.created_time,
+            lastEdited: post.last_edited_time
+        }
+
+        return postData
+
+
+    })
+  return (
+    <div>
+        <ul>
+            {postsList.map((post, index) => {
+                if (!post) return null
+                return (
+                    <li key={index}>
+                        <h2>{post.title}</h2>
+                        <p>Created: {new Date(post.created).toLocaleDateString()}</p>
+                        <p>Last Edited: {new Date(post.lastEdited).toLocaleDateString()}</p>
+                    </li>
+                )
+            })}
+        </ul>
+    </div>
+  );
+}
